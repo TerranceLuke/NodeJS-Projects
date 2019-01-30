31c20d813c7e02830e62e845973bef5fab6be4c8
TerranceLuke <TerranceLuke.pa@gmail.com>
2019-01-30 15:48:44 -0500

    Added opn and nodemon, updated srcServer to work with opn dependency.

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const path =  require('path');
-const open = require('open');
+const open = require('opn');
 
 const port = 3000;
 const app = express();
 
-app.get('/', function(req,res)
+app.get('/', function(req, res)
 {
-    res.sendFile(path.join(__dirname, '../scr/index.html'));
+    res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-//app.get()
+//app.get(/)
 
 app.listen(port, function(err)
 {
@@ -21,5 +21,6 @@ app.listen(port, function(err)
     else
     {
         open('http://localhost:' + port);
+        console.log("Server ready and listening on " + port + "...");
     }
-})
+});
